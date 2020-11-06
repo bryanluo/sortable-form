b77bfc509e3b1ca432b5bfd339d1b53ccffbd651
shijiang.luo <shijiang.luo@gziiim.com>
2020-11-06 09:57:38 +0800

    add fix drag table

--- src/components/FixForm.vue
+++ src/components/FixForm.vue
@@ -1,48 +1,50 @@
 <template>
   <div class="main">
-    <el-table 
-    :data="tableData" 
-    border 
-    align="left"
-    row-key="id">
-                    
-                       
-                      <el-table-column
-                          width="60px"
-                          label="序号"
-                          type="index" fixed="left" class-name="no-drag">
-                    </el-table-column>
-      
-                        <el-table-column v-for="(item, index) in col"
-                        :key="`col_${index}`" 
-                        :prop="dropCol[index].prop" 
-                        :label="item.label">
+    <el-table
+        :data="tableData"
+        border
+        align="left"
+        row-key="id">
 
-                     </el-table-column>
+      <!-- 序号  -->
+      <el-table-column
+          fixed="left"
+          width="60px"
+          label="序号"
+          type="index" class-name="no-drag">
+      </el-table-column>
 
-                    <el-table-column
-                     class-name="no-drag"
-                            fixed="right"
-                            label="操作"
-                            width="100">
-                            <template slot-scope="scope">
-                                <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-                                <el-button type="text" size="small">编辑</el-button>
-                            </template>
-                            </el-table-column>
+      <!-- main table  -->
+      <el-table-column v-for="(item, index) in col"
+                       :key="`col_${index}`"
+                       :prop="dropCol[index].prop"
+                       :label="item.label">
+      </el-table-column>
+
+     <!-- 操作 -->
+      <el-table-column
+          fixed="right"
+          class-name="no-drag"
+          label="操作"
+          width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
 
     </el-table>
-    
+
     <!-- header list -->
     <pre style="text-align: left">
-      {{dropCol}}
+      {{ dropCol }}
     </pre>
-    
+
     <hr>
-    
+
     <!-- data list -->
     <pre style="text-align: left">
-      {{tableData}}
+      {{ tableData }}
     </pre>
   </div>
 
@@ -111,59 +113,46 @@ export default {
       ]
     }
   },
-  mounted(){
-      this.rowDrop()
-      this.colDrag()
+  mounted() {
+    this.rowDrop()
+    this.colDrag()
   }
   ,
   methods: {
-      //行拖拽
-            rowDrop() {
-              const tbody = document.querySelector('.el-table__body-wrapper tbody')
-              const _this = this
-              Sortable.create(tbody, {
-                onEnd({ newIndex, oldIndex }) {
-                  const currRow = _this.tableData.splice(oldIndex, 1)[0]
-                  _this.tableData.splice(newIndex, 0, currRow)
-                }
-              })
-            },
-      colDrag(){
-          // 首先获取需要拖拽的dom节点
-        const el1 = document.querySelector('.el-table__header-wrapper tr')
-        Sortable.create(el1, {
-          filter: '.no-drag',
-          disabled: false, // 是否开启拖拽
-          animation: 150, // 拖拽延时，效果更好看
-          onEnd: (evt) => { // 这里主要进行数据的处理，拖拽实际并不会改变绑定数据的顺序，这里需要自己做数据的顺序更改
-            // let arr = this.dropCol; // 获取表数据
-            // arr.splice(e.newIndex, 0, arr.splice(e.oldIndex, 1)[0]); // 数据处理
-            // this.$nextTick(function () {
-            //   this.dropCol= arr; 
-            // });
-            const oldItem = this.dropCol[evt.oldIndex]
-            this.dropCol.splice(evt.oldIndex, 1)
-            this.dropCol.splice(evt.newIndex, 0, oldItem)
-          },
-          onFilter: evt => {
+    //行拖拽
+    rowDrop() {
+      const tbody = document.querySelector('.el-table__body-wrapper tbody')
+      const _this = this
+      Sortable.create(tbody, {
+        onEnd({newIndex, oldIndex}) {
+          const currRow = _this.tableData.splice(oldIndex, 1)[0]
+          _this.tableData.splice(newIndex, 0, currRow)
+        }
+      })
+    },
+    colDrag() {
+      // 首先获取需要拖拽的dom节点
+      const el1 = document.querySelector('.el-table__header-wrapper tr')
+      Sortable.create(el1, {
+        filter: '.no-drag',
+        disabled: false, // 是否开启拖拽
+        animation: 150, // 拖拽延时，效果更好看
+        onEnd: (evt) => {
+          // let arr = this.dropCol; // 获取表数据
+          // arr.splice(e.newIndex, 0, arr.splice(e.oldIndex, 1)[0]); // 数据处理
+          // this.$nextTick(function () {
+          //   this.dropCol= arr;
+          // });
+          const oindex = evt.oldIndex - 1;
+          const oldItem = this.dropCol[oindex]
+          this.dropCol.splice(oindex, 1)
+          this.dropCol.splice(evt.newIndex - 1, 0, oldItem)
+        },
+        onFilter: evt => {
           console.log('evt:', evt)
         }
-        });
-       },
-    //   colDrag() {
-    //           const wrapperTr = document.querySelector('.el-table__header-wrapper tr')
-    //           this.sortable = Sortable.create(wrapperTr, {
-    //             animation: 180,
-    //             delay: 0,
-    //             filter: ".cannotDrag",
-    //             draggable: ".canDrag",
-    //             onEnd: evt => {
-    //               const oldItem = this.dropCol[evt.oldIndex]
-    //               this.dropCol.splice(evt.oldIndex, 1)
-    //               this.dropCol.splice(evt.newIndex, 0, oldItem)
-    //             }
-    //           })
-    //         }
+      });
+    },
   }
 }
 </script>
